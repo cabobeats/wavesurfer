f50c4ac06d1bb9bfd4516d2b453ec90f93f5f526
cabobeats <61565708+cabobeats@users.noreply.github.com>
2023-09-12 12:50:25 -0600

    audioPlayer.js

--- audio1.js
+++ audio1.js
@@ -1,25 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
-        const audioSrc = "https://dl.dropboxusercontent.com/s/lyg3e2m3cq12t5l/korupt%202.mp3";
-        const wavesurfer = WaveSurfer.create({
-            container: document.querySelector('.audio-container #waveform'),
-            waveColor: "#ffffff",
-            progressColor: "#fe2d0e",
-            cursorColor: "#ffffff",
-            barWidth: 2,
-            height: 64,
-            minPxPerSec: 50
-        });
-        wavesurfer.load(audioSrc);
-        wavesurfer.on("ready", function () {
-            const playButton = document.querySelector(".audio-container .js-audio-toggle");
-            playButton.addEventListener("click", function () {
-                wavesurfer.playPause();
-                playButton.querySelector(".play-icon").classList.toggle("fa-play");
-                playButton.querySelector(".play-icon").classList.toggle("fa-pause");
-                playButton.querySelector(".stop-icon").classList.toggle("fa-play");
-                playButton.querySelector(".stop-icon").classList.toggle("fa-pause");
+        var waveformContainers = document.querySelectorAll(".waveform-container");
+        waveformContainers.forEach(function(container, index) {
+            var audioSrc = container.getAttribute("data-audio-src");
+            
+            var wavesurfer = WaveSurfer.create({
+                container: container,
+                waveColor: "#f01d1d",
+                progressColor: "#fe2d0e",
+                cursorColor: "#000000",
+                barWidth: 2,
+                height: 64,
+                minPxPerSec: 50
+            });
+
+            wavesurfer.load(audioSrc);
+
+            wavesurfer.on("ready", function() {
+                var playButtons = document.querySelectorAll(".js-audio-toggle");
+                var playButton = playButtons[index];
+                playButton.addEventListener("click", function() {
+                    wavesurfer.playPause();
+                    playButton.querySelector(".play-icon").classList.toggle("fa-play");
+                    playButton.querySelector(".play-icon").classList.toggle("fa-pause");
+                    playButton.querySelector(".stop-icon").classList.toggle("fa-play");
+                    playButton.querySelector(".stop-icon").classList.toggle("fa-pause");
+                });
             });
         });
-    }, 500); // 500ms delay
+    }, 500);
 });
